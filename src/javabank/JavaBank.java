750d5203c0a5010463d73b472e0045ceec638659
Mathushanan Mahendrarajah <posttoday0@gmail.com>
2023-08-23 17:15:08 +0530

    Database connection tested

---
+++ src/javabank/JavaBank.java
@@ -0,0 +1,17 @@
+
+package javabank;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JavaBank {
+
+   
+    public static void main(String[] args) throws SQLException {
+        
+        Connection c=DriverManager.getConnection("jdbc:mysql://localhost:3306/mysql?zeroDateTimeBehavior=CONVERT_TO_NULL","root","");
+        System.out.print(c);
+       
+    }
+    
+}
